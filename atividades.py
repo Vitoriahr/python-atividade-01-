d5d93acf357b9be2ec038b2e8cf5dc6a037dcb6a
Vitoriahr <145279784+Vitoriahr@users.noreply.github.com>
2023-09-20 12:16:43 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+
+num1=float(input("digite o primeiro numero: "))
+num2=float(input("digite o primeiro numero: "))
+
+
+soma=float(num1+num2)
+subtracao=float(num1-num2)
+multiplicacao=float(num1*num2)
+divisao=float(num1/num2)
+
+
+print(f"soma:{soma}")
+print(f"subtraçâo:{subtracao}")
+print(f"multiplicacao:{multiplicacao}") 
+print(f"divisao:{divisao}")
+    
